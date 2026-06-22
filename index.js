1da492ace57def8919532b23da6093976ae2a0b7
KingDsquared <Kaanadiguzel040@gmail.com>
2026-06-22 17:28:48 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,39 +1,59 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const { Client, GatewayIntentBits } = require('discord.js');
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const WELCOME_CHANNEL_NAME = "welcome";
 
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
     GatewayIntentBits.GuildMembers
   ]
 });
 
-client.once('ready', () => {
-  console.log(`${client.user.tag} is online!`);
+client.on("ready", () => {
+  console.log("BuddyNew is online as " + client.user.tag);
 });
 
-client.on('guildMemberAdd', async (member) => {
+client.on("guildMemberAdd", (member) => {
   const channel = member.guild.channels.cache.find(
-    c => c.name === 'welcome'
+    ch => ch.name === WELCOME_CHANNEL_NAME
   );
 
   if (!channel) return;
 
-  await channel.send(`
-👋 Welcome to **${member.guild.name}**, ${member}!
+  channel.send(
+    `Welcome ${member} to **${member.guild.name}**! 👋\n\n` +
+    `Please read the rules, introduce yourself, and have fun.\n\n` +
+    `Start here:\n` +
+    `1. Read the rules\n` +
+    `2. Pick your roles if the server has role channels\n` +
+    `3. Say hello in chat\n` +
+    `4. Ask staff if you need help`
+  );
+});
 
-Before applying, please make sure your **Path of Exile account is set to Public** so our officers can review your profile.
+client.on("messageCreate", (message) => {
+  if (message.author.bot) return;
 
-Once your account has been reviewed and approved, head over to **#apply** and submit an application for:
+  const msg = message.content.toLowerCase();
 
-⚔️ Path of Exile 1
-⚔️ Path of Exile 2
+  if (msg === "!ping") {
+    message.reply("Pong!");
+  }
 
-Please include your Path of Exile account name.
+  if (msg === "!help") {
+    message.reply("Commands: `!ping`, `!help`, `!welcome-test`");
+  }
 
-Welcome to the community, Exile!
-`);
+  if (msg === "!welcome-test") {
+    message.channel.send(
+      `Welcome ${message.author} to **${message.guild.name}**! 👋\n\n` +
+      `Please read the rules, introduce yourself, and have fun.`
+    );
+  }
 });
 
 client.login(process.env.TOKEN);
