574a7f5fd6585a604b082a3ecc2eccaf591ae39a
KingDsquared <Kaanadiguzel040@gmail.com>
2026-06-22 20:24:43 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,102 +1,83 @@
 const roles = ["Tank", "Healer", "Melee DPS", "Ranged DPS", "Flex"];
 
 const wowClasses = [
-  "Death Knight",
-  "Demon Hunter",
-  "Druid",
-  "Evoker",
-  "Hunter",
-  "Mage",
-  "Monk",
+  "Warrior",
   "Paladin",
-  "Priest",
+  "Hunter",
   "Rogue",
+  "Priest",
   "Shaman",
+  "Mage",
   "Warlock",
-  "Warrior"
+  "Druid"
 ];
 
 const classEmoji = {
-  "Death Knight": "🦴",
-  "Demon Hunter": "😈",
-  "Druid": "🌿",
-  "Evoker": "🐉",
-  "Hunter": "🏹",
-  "Mage": "❄️",
-  "Monk": "🍃",
-  "Paladin": "✨",
-  "Priest": "☀️",
-  "Rogue": "🗡️",
-  "Shaman": "⚡",
-  "Warlock": "🔥",
-  "Warrior": "⚔️"
+  Warrior: "⚔️",
+  Paladin: "✨",
+  Hunter: "🏹",
+  Rogue: "🗡️",
+  Priest: "☀️",
+  Shaman: "⚡",
+  Mage: "❄️",
+  Warlock: "🔥",
+  Druid: "🌿"
 };
 
 const specs = {
-  "Death Knight": [
-    { spec: "Blood", role: "Tank" },
-    { spec: "Frost", role: "Melee DPS" },
-    { spec: "Unholy", role: "Melee DPS" }
+  Warrior: [
+    { spec: "Protection", role: "Tank" },
+    { spec: "Arms", role: "Melee DPS" },
+    { spec: "Fury", role: "Melee DPS" }
   ],
-  "Demon Hunter": [
-    { spec: "Vengeance", role: "Tank" },
-    { spec: "Havoc", role: "Melee DPS" }
-  ],
-  Druid: [
-    { spec: "Guardian", role: "Tank" },
-    { spec: "Restoration", role: "Healer" },
-    { spec: "Balance", role: "Ranged DPS" },
-    { spec: "Feral", role: "Melee DPS" }
-  ],
-  Evoker: [
-    { spec: "Preservation", role: "Healer" },
-    { spec: "Devastation", role: "Ranged DPS" },
-    { spec: "Augmentation", role: "Ranged DPS" }
-  ],
-  Hunter: [
-    { spec: "Beast Mastery", role: "Ranged DPS" },
-    { spec: "Marksmanship", role: "Ranged DPS" },
-    { spec: "Survival", role: "Melee DPS" }
-  ],
-  Mage: [
-    { spec: "Arcane", role: "Ranged DPS" },
-    { spec: "Fire", role: "Ranged DPS" },
-    { spec: "Frost", role: "Ranged DPS" }
-  ],
-  Monk: [
-    { spec: "Brewmaster", role: "Tank" },
-    { spec: "Mistweaver", role: "Healer" },
-    { spec: "Windwalker", role: "Melee DPS" }
-  ],
+
   Paladin: [
     { spec: "Protection", role: "Tank" },
     { spec: "Holy", role: "Healer" },
     { spec: "Retribution", role: "Melee DPS" }
   ],
-  Priest: [
-    { spec: "Holy", role: "Healer" },
-    { spec: "Discipline", role: "Healer" },
-    { spec: "Shadow", role: "Ranged DPS" }
+
+  Hunter: [
+    { spec: "Beast Mastery", role: "Ranged DPS" },
+    { spec: "Marksmanship", role: "Ranged DPS" },
+    { spec: "Survival", role: "Ranged DPS" }
   ],
+
   Rogue: [
     { spec: "Assassination", role: "Melee DPS" },
-    { spec: "Outlaw", role: "Melee DPS" },
+    { spec: "Combat", role: "Melee DPS" },
     { spec: "Subtlety", role: "Melee DPS" }
   ],
+
+  Priest: [
+    { spec: "Discipline", role: "Healer" },
+    { spec: "Holy", role: "Healer" },
+    { spec: "Shadow", role: "Ranged DPS" }
+  ],
+
   Shaman: [
     { spec: "Restoration", role: "Healer" },
     { spec: "Elemental", role: "Ranged DPS" },
     { spec: "Enhancement", role: "Melee DPS" }
   ],
+
+  Mage: [
+    { spec: "Arcane", role: "Ranged DPS" },
+    { spec: "Fire", role: "Ranged DPS" },
+    { spec: "Frost", role: "Ranged DPS" }
+  ],
+
   Warlock: [
     { spec: "Affliction", role: "Ranged DPS" },
     { spec: "Demonology", role: "Ranged DPS" },
     { spec: "Destruction", role: "Ranged DPS" }
   ],
-  Warrior: [
-    { spec: "Protection", role: "Tank" },
-    { spec: "Arms", role: "Melee DPS" },
-    { spec: "Fury", role: "Melee DPS" }
+
+  Druid: [
+    { spec: "Feral Tank", role: "Tank" },
+    { spec: "Feral DPS", role: "Melee DPS" },
+    { spec: "Restoration", role: "Healer" },
+    { spec: "Balance", role: "Ranged DPS" }
   ]
 };
 
