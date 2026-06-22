39778c2fd5821f6e696ad1dd461b7ea6ef6befb4
KingDsquared <Kaanadiguzel040@gmail.com>
2026-06-22 20:01:13 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -42,58 +42,58 @@ const specs = {
     { spec: "Vengeance", role: "Tank" },
     { spec: "Havoc", role: "Melee DPS" }
   ],
-  "Druid": [
+  Druid: [
     { spec: "Guardian", role: "Tank" },
     { spec: "Restoration", role: "Healer" },
     { spec: "Balance", role: "Ranged DPS" },
     { spec: "Feral", role: "Melee DPS" }
   ],
-  "Evoker": [
+  Evoker: [
     { spec: "Preservation", role: "Healer" },
     { spec: "Devastation", role: "Ranged DPS" },
     { spec: "Augmentation", role: "Ranged DPS" }
   ],
-  "Hunter": [
+  Hunter: [
     { spec: "Beast Mastery", role: "Ranged DPS" },
     { spec: "Marksmanship", role: "Ranged DPS" },
     { spec: "Survival", role: "Melee DPS" }
   ],
-  "Mage": [
+  Mage: [
     { spec: "Arcane", role: "Ranged DPS" },
     { spec: "Fire", role: "Ranged DPS" },
     { spec: "Frost", role: "Ranged DPS" }
   ],
-  "Monk": [
+  Monk: [
     { spec: "Brewmaster", role: "Tank" },
     { spec: "Mistweaver", role: "Healer" },
     { spec: "Windwalker", role: "Melee DPS" }
   ],
-  "Paladin": [
+  Paladin: [
     { spec: "Protection", role: "Tank" },
     { spec: "Holy", role: "Healer" },
     { spec: "Retribution", role: "Melee DPS" }
   ],
-  "Priest": [
+  Priest: [
     { spec: "Holy", role: "Healer" },
     { spec: "Discipline", role: "Healer" },
     { spec: "Shadow", role: "Ranged DPS" }
   ],
-  "Rogue": [
+  Rogue: [
     { spec: "Assassination", role: "Melee DPS" },
     { spec: "Outlaw", role: "Melee DPS" },
     { spec: "Subtlety", role: "Melee DPS" }
   ],
-  "Shaman": [
+  Shaman: [
     { spec: "Restoration", role: "Healer" },
     { spec: "Elemental", role: "Ranged DPS" },
     { spec: "Enhancement", role: "Melee DPS" }
   ],
-  "Warlock": [
+  Warlock: [
     { spec: "Affliction", role: "Ranged DPS" },
     { spec: "Demonology", role: "Ranged DPS" },
     { spec: "Destruction", role: "Ranged DPS" }
   ],
-  "Warrior": [
+  Warrior: [
     { spec: "Protection", role: "Tank" },
     { spec: "Arms", role: "Melee DPS" },
     { spec: "Fury", role: "Melee DPS" }
